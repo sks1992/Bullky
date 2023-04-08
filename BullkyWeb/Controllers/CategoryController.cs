c5a36dd20fbdcff1f6524d4f2d94dc1ca284276c
Sandeep <sandeepsk15892>
2023-04-08 10:45:16 +0530

    get Category data from db

---
+++ BullkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using BullkyWeb.Data;
+using BullkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BullkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //access db because dependency injection when we add applicationDvContext
+        //to progtam.cs we can access it as DI.
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            //get catrgory data from db
+            List<Category> categoryList = _db.Categories.ToList();
+            //send category list to view
+            return View();
+        }
+    }
+}
+    
---
+++ BullkyWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BullkyWeb.Models
+{
+    public class Category
+    {
+        //if we add id in class name or write id it automatic think it as primary key
+        //[Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
